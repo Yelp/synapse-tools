ea75e8731be70b66938bf25b78ce4ecdbd1b662e
Avadhut Phatarpekar <avadhutp@yelp.com>
2018-10-16 07:47:17 -0700

    Incorporating review feedback

--- src/synapse_tools/lua_scripts/add_source_header.lua
+++ src/synapse_tools/lua_scripts/add_source_header.lua
@@ -1,55 +1,24 @@
 map = {}
-map_file = ''
-
--- Get the path to the haproxy.cfg file
-function get_config_filename(cmdline)
-  local found = false
-  for s in string.gmatch(cmdline, '%g+') do
-    if s == '-f' then
-      found = true
-    elseif found then
-      return s
-    end
-  end
-end
-
--- Get the map_file path from haproxy.cfg's global stanza
-function get_map_file(config_file)
-   fh, err = io.open(config_file)
-   if err then
-     core.log(core.err, 'Cannot open the map file at ' .. config_file .. '. This will cause service authentication to fail!')
-   end
-
-  while true do
-     ln = fh:read()
-    if ln == nil then break end
-    if ln:find('setenv map_file') then
-       return ln:match('%S+/.*$')
-    end
-  end
-end
-
--- Get the cmdline used to run the haproxy process, because this cmdline
--- will also have the path to haproxy.cfg
-function get_cmdline()
-  local f = io.open('/proc/self/cmdline', "rb")
-  local cmdline = f:read("*all")
-  f:close()
-
-  return cmdline
-end
+map_file = nil
+refresh_interval = nil
 
 function refresh_map()
   map = Map.new(map_file, Map.str)
 end
 
+function init_vars()
+  map_file = os.getenv('map_file')
+  if map_file == nil then
+     core.log(core.err, 'map_file variable not set! This will cause authentication to fail for some requests.')
+   end
+
+  refresh_interval = os.getenv('map_refresh_interval')
+end
+
 -- This is run soon after haproxy parses haproxy.cfg. We will
 -- load the ip_to_svc map for the first time here.
 core.register_init(function()
-  local cmdline = get_cmdline()
-  local config_file = get_config_filename(cmdline)
-  map_file = get_map_file(config_file)
-
+  init_vars()
   refresh_map()
 end)
 
@@ -58,7 +27,7 @@ end)
 core.register_task(function()
   while true do
     refresh_map()
-    core.sleep(5)
+    core.sleep(refresh_interval)
   end
 end)
 
