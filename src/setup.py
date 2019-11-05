dba1aa0dfeb70412d5db367da39e371841916228
Matthew Bentley <bentley@yelp.com>
2019-11-05 14:47:28 -0800

    Released 0.14.0 via make release

--- src/setup.py
+++ src/setup.py
@@ -14,7 +14,7 @@ def get_install_requires():
 
 setup(
     name='synapse-tools',
-    version='0.13.33',
+    version='0.14.0',
     provides=['synapse_tools'],
     author='John Billings',
     author_email='billings@yelp.com',
