662d41346d352635ea89f56a164f7f4d4e7c283d
Chris Kuehl <ckuehl@yelp.com>
2020-08-11 11:28:35 -0700

    v0.15.0

--- src/setup.py
+++ src/setup.py
@@ -14,7 +14,7 @@ def get_install_requires():
 
 setup(
     name='synapse-tools',
-    version='0.14.3',
+    version='0.15.0',
     provides=['synapse_tools'],
     author='John Billings',
     author_email='billings@yelp.com',
